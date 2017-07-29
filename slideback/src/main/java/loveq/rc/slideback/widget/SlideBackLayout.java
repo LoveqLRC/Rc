2fe1a9b3c781480e4813655508b4d408dc5ad250
loveqRc <664215432@qq.com>
2017-07-29 14:02:19 +0800

    添加变量

--- slideback/src/main/java/loveq/rc/slideback/widget/SlideBackLayout.java
+++ slideback/src/main/java/loveq/rc/slideback/widget/SlideBackLayout.java
@@ -6,14 +6,31 @@ import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.FrameLayout;
 
+import loveq.rc.slideback.SlideConfig;
+import loveq.rc.slideback.callback.OnInternalStateListener;
+
 /**
  * Created by rc on 2017/7/29.
  * Description:
  */
 
 public class SlideBackLayout extends FrameLayout {
+    private View mContentView;
+    private View mPreContentView;
+    private Drawable mPreDecorViewDrawable;
+    private OnInternalStateListener mOnInternalStateListener;
+
+
     public SlideBackLayout(@NonNull Context context, View contentView, View preContentView,
-                           Drawable preDecorViewDrawable) {
+                           Drawable preDecorViewDrawable, SlideConfig config,
+                           OnInternalStateListener onInternalStateListener) {
         super(context);
+
+        mContentView = contentView;
+        mPreContentView = preContentView;
+        mPreDecorViewDrawable = preDecorViewDrawable;
+        mOnInternalStateListener = onInternalStateListener;
+
+
     }
 }
